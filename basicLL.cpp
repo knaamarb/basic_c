d984368138c38433480c013fd210f0bc18451a6d
knaamarb <knaamarb@gmail.com>
2025-03-20 13:46:24 +0700

    Double Linked List
    node, newNode, popTail, popHead, inHead, inTail, printLL.

---
+++ basicLL.cpp
@@ -0,0 +1,71 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+struct node
+{
+    int priotity;
+    struct node *prev, *next;
+};
+struct node *head = NULL, *tail = NULL;
+
+struct node *newNode(int priority){
+    struct node *new_node = (struct node*)malloc(sizeof(struct node));
+    new_node->priotity = priority;
+    new_node->next = NULL;
+    new_node->prev = NULL;
+    return new_node;
+}
+
+void inHead(int priority){
+    struct node *tmp = newNode(priority);
+    if(head == NULL){
+        head = tail = tmp;
+    }else{
+        head->prev = tmp;
+        tmp->next = head;
+        head = tmp;
+    }
+}
+
+void inTail(int priority){
+    struct node *tmp = newNode(priority);
+    if(tail==NULL){
+        tail = head = tmp;
+    }else{
+        tail->next = tmp;
+        tmp->prev = tail;
+        tail = tmp;
+    }
+}
+
+void popHead(){
+    struct node *tmp = head;
+    head = head->next;
+    head->prev = NULL;
+    free(tmp);
+}
+
+void popTail(){
+    struct node *tmp = tail;
+    tail = tail->prev;
+    tail->next = NULL;
+    free(tmp);
+}
+
+
+void printAll(){
+    struct node *tmp = head;
+    while(tmp!=NULL){
+        printf("%d ",tmp->priotity);
+        tmp = tmp->next;
+    }
+}
+
+
+int main(){
+    inTail(10);
+    inTail(20);
+    printAll();
+    return 0;
+}
